e05afbf2c18b7a36fc48167d398abfc5657e2942
roxanne7777 <0533864@cmontmorency.qc.ca>
2024-10-17 23:31:58 -0400

    s2c1 modifications css

--- index.php
+++ index.php
@@ -16,7 +16,7 @@
 </head>
 
 <body>
-  <header>
+  <header class="entete">
     <section class="global">
       <h1>31W</h1>
       <nav>
@@ -35,18 +35,20 @@
       </form>
     </section>
   </header>
-  <main>
+  <main class="principal">
     <section class="global">
-      <h2>Accueil</h2>
-      <p>
-        Bienvenue sur 31W Lorem ipsum dolor sit, amet consectetur adipisicing
-        elit. Magnam, quaerat eius aspernatur dolor veniam sit adipisci
-        reiciendis totam natus temporibus. Saepe iste consectetur officia
-        animi voluptatem laudantium ab hic inventore!
-      </p>
+        <h2>Accueil</h2>
+        <?php if (have_posts()): ?>
+            <?php while (have_posts()): the_post(); ?>
+                <article class="principal__article">
+                    <h2><?php the_title(); ?></h2>
+                    <p><?php echo wp_trim_words( get_the_excerpt(), 20, null ); ?></p>
+                </article>
+            <?php endwhile; ?>
+        <?php endif; ?>
     </section>
   </main>
-  <footer>
+  <footer class="pied">
     <section class="global">
       <div>1</div>
       <div>2</div>
